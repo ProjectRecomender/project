cebb33002a26573b08516344767ce891ff53c9f2
unknown <sarthakagrawal927@gmail.com>
2020-09-05 13:19:03 +0530

    heroku attempt

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 const path = require("path");
 const morgan = require("morgan");
 
-if (process.env.MODE === "PROD") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build", { maxAge: 8640000 }));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
- 
 app.use("/recommend", require("./routes/recommend"));
 
 app.get("/",(req, res)=>{
